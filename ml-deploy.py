c606cb9a6e2f4458fd1a6fefeb91f77f072a4ae8
LaTlez <95763190+LaTlez@users.noreply.github.com>
2024-05-02 10:37:58 +0700

    Update ml-deploy.py 3

--- ml-deploy.py
+++ ml-deploy.py
@@ -4,30 +4,15 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 
-st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+st.set_page_config(page_title="ChatMATE - Chat Multi-purpose AI Technology")
 
-# Sidebar contents
-with st.sidebar:
-    st.title('🤗💬 HugChat App')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [HugChat](https://github.com/Soulter/hugging-chat-api)
-    - [OpenAssistant/meta-llama/Meta-Llama-3-70B-Instruct](https://hf.co/chat/assistant/6630cebfe39f9d880056b473) LLM model
-    
-    💡 Note: No API key required!
-    ''')
-    add_vertical_space(5)
-    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
-
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
-## past stores User's questions
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
+# Generate empty lists for bot_response and user_input.
+## bot_response stores AI generated responses
+if 'bot_response' not in st.session_state:
+    st.session_state['bot_response'] = ["I'm ChatMATE, How may I help you?"]
+## user_input stores User's questions
+if 'user_input' not in st.session_state:
+    st.session_state['user_input'] = ['Hi!']
 
 # Layout of input/response containers
 input_container = st.container()
@@ -36,17 +21,17 @@ response_container = st.container()
 
 # User input
 ## Function for taking user provided prompt as input
-def get_text():
+def get_input():
     input_text = st.text_input("You: ", "", key="input")
     return input_text
 ## Applying the user input box
 with input_container:
-    user_input = get_text()
+    user_input = get_input()
 
 # Response output
 ## Function for taking user prompt as input followed by producing AI generated responses
 def generate_response(prompt):
-    chatbot = hugchat.ChatBot()
+    chatbot = hugchat.ChatBot(cookie_path="cookies.json")
     response = chatbot.chat(prompt)
     return response
 
@@ -54,10 +39,10 @@ def generate_response(prompt):
 with response_container:
     if user_input:
         response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+        st.session_state.user_input.append(user_input)
+        st.session_state.bot_response.append(response)
+
+    if st.session_state['bot_response']:
+        for i in range(len(st.session_state['bot_response'])):
+            message(st.session_state['user_input'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state['bot_response'][i], key=str(i))
